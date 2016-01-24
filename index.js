e060138c983d41f0d95016791c7b3e0703f9d12c
Jeremy Osborne <jeremywosborne@gmail.com>
2016-01-24 12:04:56 -0800

    Some clean up and separation.

--- index.js
+++ index.js
@@ -1,72 +1,22 @@
-#!/usr/bin/env node
-/* jshint undef:true, unused:true, node:true */
-
-try {
-    var request = require("request");
-    var toMarkdown = require("to-markdown").toMarkdown;
-    var argv = require("minimist")(process.argv.slice(2));
-    var meta = require("./package.json");
-} catch (e) {
-    console.error("Make sure to `npm install` before running this code.");
-    console.error("Here's the error that happened during module import:", e, "\n\n");
-    process.exit(1);
-}
+var request = require("request");
+var toMarkdown = require("to-markdown").toMarkdown;
 
 
-var verboseFlag = argv.v;
-var verbose = function() {
-    if (verboseFlag) {
-        console.error.apply(console, arguments);
+// Determine if an particular transform exists.
+// Synchronous code, raises exception if it does not exist.
+exports.xformExists = function(xform) {
+    try {
+        xform = require("./xforms/" + xform);
+        return true;
+    } catch(e) {
+        return false;
     }
 };
 
 
-var url = argv._[0];
-if (!url) {
-    var usage = `\n${meta.name} ${meta.version}
-
-Usage:
-
-    html-to-markdown -v [--xform=some-style] <url>
-
--v
-    verbose mode. non-content comments to stderr (content only ever to stdout)
-
---xform
-    none (default)
-    wikipedia
-`;
-    console.error(usage);
-    process.exit(1);
-}
-
-// xform is the function, style is the argument string.
-var xform;
-var style = argv.xform;
-if (style) {
-    try {
-        xform = require("./xforms/" + style);
-        verbose("using xform style:", style);
-    } catch(e) {
-        console.error("Could not find xform style:", style);
-        console.error("Exiting.");
-        process.exit(1);
-    }
-} else {
-    style = "none";
-    xform = require("./xforms/" + style);
-    verbose("default xform style:", style);
-}
-
-
-
-request(url, function(err, res, body) {
-    if (err) {
-        console.error("Sorry, could not retrieve the page:", url);
-        console.error("Error returned:", err);
-        process.exit(1);
-    }
-
-    // Output.
-    console.log(toMarkdown(xform(body)));
-});
+exports.fetch = function(url, xform, callback) {
+    var xformFn = require("./xforms/" + xform);
+    request(url, function(err, res, body) {
+        callback(err, toMarkdown(xformFn(body)));
+    });
+};
---
+++ script.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+try {
+    var htmlToMarkdown = require("./index");
+    var meta = require("./package.json");
+    var argv = require("minimist")(process.argv.slice(2));
+} catch (e) {
+    console.error("Problems during import of script dependencies. Here's the error:\n", e, "\n\n");
+    process.exit(1);
+}
+
+
+
+var url = argv._[0];
+if (!url) {
+    var usage = `
+
+${meta.name} ${meta.version}
+
+Usage:
+
+    html-to-markdown -v [--xform=some-style] <url>
+
+-v
+    verbose mode. non-content comments to stderr (content only ever to stdout)
+
+--xform
+    none (default)
+    wikipedia
+`;
+    console.error(usage);
+    process.exit(1);
+}
+
+
+
+var verboseFlag = argv.v;
+var verbose = function() {
+    if (verboseFlag) {
+        console.error.apply(console, arguments);
+    }
+};
+
+
+
+// xform is the function, style is the argument string.
+var xform = argv.xform;
+if (xform) {
+    if (htmlToMarkdown.xformExists(xform)) {
+        verbose("using xform:", xform);
+    } else {
+        console.error("Could not find xform:", xform, "\nExiting.");
+        process.exit(1);
+    }
+} else {
+    xform = "none";
+    verbose("using default xform named", xform);
+}
+
+
+// If we haven't exited, give it a try:
+htmlToMarkdown.fetch(url, xform, function(err, md) {
+    if (err) {
+        console.error("Sorry, could not retrieve the page:", url);
+        console.error("Error returned:", err);
+        process.exit(1);
+    }
+
+    // Output.
+    console.log(md);
+});
